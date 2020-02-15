af8c9ad1277e011812d2eb857872e6b00ca3f149
hirarira <hirarira617@gmail.com>
2020-02-15 16:50:55 +0900

    reduxの原型を追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { create } from 'domain';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function counter(state:any, action:any) {
+	switch(action.type){
+		case 'INCREMENT':
+			return {
+				counter: state.counter + 1,
+				message: 'INCREMENT'
+			};
+		default:
+			return state;
+	}
+}
+
+const store = createStore(counter);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>, 
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
