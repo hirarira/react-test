5089cd4843e36dffeb7771018f0ea05d6acf6cd5
hirarira <hirarira617@gmail.com>
2020-02-16 03:43:56 +0900

    ボタンを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h3>カウンター</h3>
+      <Box component="span" m={1}>
+        <Button variant="contained" color="primary">
+          INCREMENT
+        </Button>
+        <Button variant="contained" color="secondary">
+          DECREMENT
+        </Button>
+      </Box>
       </header>
     </div>
   );
